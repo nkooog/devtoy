d72c5428d4eccbbc54fe90d5c85a6ff5c8b2ec2b
Jy_Park <nkooog@naver.com>
2025-03-21 13:32:49 +0900

    toy project init

---
+++ BE/gw-service/src/main/java/org/example/gwserver/GwServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.gwserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwServerApplication.class, args);
+	}
+
+}
---
+++ BE/gw-service/src/test/java/org/example/gwserver/GwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gwserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
