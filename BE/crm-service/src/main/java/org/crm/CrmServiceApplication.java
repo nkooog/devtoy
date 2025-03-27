cbed18e7291d4df2d72b1f6ecc776277205556de
Jy_Park <nkooog@naver.com>
2025-03-27 15:37:20 +0900

    elh, crm 임시 DB exclude

--- BE/crm-service/src/main/java/org/crm/CrmServiceApplication.java
+++ BE/crm-service/src/main/java/org/crm/CrmServiceApplication.java
@@ -2,8 +2,9 @@ package org.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CrmServiceApplication {
 
 	public static void main(String[] args) {
