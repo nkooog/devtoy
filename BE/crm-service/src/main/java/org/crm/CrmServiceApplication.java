842acfd82bfa205d1b7dafa272f1c640f05efd64
Jy_Park <nkooog@naver.com>
2025-03-28 16:53:22 +0900

    css,image volumn 추가(진행중)

--- BE/crm-service/src/main/java/org/crm/CrmServiceApplication.java
+++ BE/crm-service/src/main/java/org/crm/CrmServiceApplication.java
@@ -3,9 +3,16 @@ package org.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class CrmServiceApplication {
+public class CrmServiceApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CrmServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmServiceApplication.class, args);
