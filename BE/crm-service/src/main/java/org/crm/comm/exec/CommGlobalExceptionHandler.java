f98e00cb32e685ac33c466f993116ee319d95b21
Jy_Park <nkooog@naver.com>
2025-04-07 15:20:56 +0900

    . log 추가

--- BE/crm-service/src/main/java/org/crm/comm/exec/CommGlobalExceptionHandler.java
+++ BE/crm-service/src/main/java/org/crm/comm/exec/CommGlobalExceptionHandler.java
@@ -2,25 +2,23 @@ package org.crm.comm.exec;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.crm.comm.valid.CommValidation;
 import org.json.simple.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.Locale;
 
-@RestControllerAdvice
+@Slf4j
+@ControllerAdvice
 public class CommGlobalExceptionHandler {
-
-	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	private CommValidation validation;
 	private ObjectMapper objectMapper;
