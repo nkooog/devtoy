d0e3f4cf5f6d9574f1c07f285c4405f962d8be4c
Jy_Park <nkooog@naver.com>
2025-04-08 16:08:03 +0900

    . header 누락 시 리다이렉트 추가(토큰 만료 검증은 추가 예정)

--- BE/gw-service/src/main/java/org/gateway/filter/GlobalFilter.java
+++ BE/gw-service/src/main/java/org/gateway/filter/GlobalFilter.java
@@ -1,12 +1,9 @@
 package org.gateway.filter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.gateway.config.PathsToExclude;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -14,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
@@ -25,13 +21,13 @@ import java.util.List;
 @Slf4j
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
 
+	@Value("${ncrm.redirect}")
+	private String redirectUrl;
 	private PathsToExclude pathsToExclude;
-	private ObjectMapper objectMapper;
 
-	public GlobalFilter(PathsToExclude pathsToExclude, ObjectMapper objectMapper) {
+	public GlobalFilter(PathsToExclude pathsToExclude) {
 		super(Config.class);
 		this.pathsToExclude = pathsToExclude;
-		this.objectMapper = objectMapper;
 	}
 
 	@Override
@@ -41,7 +37,7 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 			ServerHttpResponse response = exchange.getResponse();
 			log.debug("Global Filter baseMessage: -> {}", config.getBaseMessage());
 
-			// 인증을 건너뛰어야 할 경로를 체크 (예: /bcs/crm/lgin/** 등)
+			// 인증을 건너뛰어야 할 경로를 체크 (예: /bcs/crm/lgin/** 등 yml > spring.cloud.gateway 정의)
 			if (isPathExcluded(request.getURI().getPath())) {
 				log.debug("Skipping authentication for path: {}", request.getURI().getPath());
 			} else {
@@ -49,14 +45,17 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 				if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)
 					&& !exchange.getRequest().getCookies().containsKey(HttpHeaders.AUTHORIZATION)) {
 
-					if (!exchange.getRequest().getURI().getPath().equals("/bcs/crm/auth")) {
-						URI redirectUri = UriComponentsBuilder.fromUriString("/bcs/crm/auth")
-								.build().toUri();
-						exchange.getResponse().setStatusCode(HttpStatus.FOUND);
-						exchange.getResponse().getHeaders().setLocation(redirectUri);
-						return exchange.getResponse().setComplete();
-					}
+					log.debug(" path ::: {}", exchange.getRequest().getURI().getPath());
+					log.debug(" path check ::: {}", exchange.getRequest().getURI().getPath().equals(this.redirectUrl));
 
+					URI redirectUri = UriComponentsBuilder.fromUriString(this.redirectUrl)
+							.build().toUri();
+					exchange.getResponse().setStatusCode(HttpStatus.FOUND);
+					exchange.getResponse().getHeaders().setLocation(redirectUri);
+					return exchange.getResponse().setComplete();
+
+				}else{
+					//TODO: 인증 header는 있지만 토큰이 만료 된 경우 추가 로직
 				}
 			}
 
@@ -70,6 +69,7 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 	private boolean isPathExcluded(String path) {
 		List<String> excludedPaths = this.pathsToExclude.getPathsToExclude();
 		for (String excludedPath : excludedPaths) {
+			log.debug(" isPathExcluded path : {}", excludedPath);
 			if (path.startsWith(excludedPath)) {
 				return true;  // 해당 경로는 인증을 건너뛴다
 			}
